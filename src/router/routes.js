83c4aa73163a905ef1ae0bb3c7929cfb94d89c0a
Marc Pont <mpont91@gmail.com>
2022-08-31 15:14:19 +0200

    Clean default scaffold. Set one SPA

--- src/router/routes.js
+++ src/router/routes.js
@@ -2,14 +2,20 @@ const routes = [
   {
     path: "/",
     component: () => import("layouts/MainLayout.vue"),
-    children: [{ path: "", component: () => import("pages/IndexPage.vue") }],
+    children: [
+      {
+        name: "index",
+        path: "",
+        component: () => import("pages/IndexPage.vue"),
+      },
+    ],
   },
 
   // Always leave this as last one,
   // but you can also remove it
   {
     path: "/:catchAll(.*)*",
-    component: () => import("pages/ErrorNotFound.vue"),
+    redirect: { name: "index" },
   },
 ];
 
