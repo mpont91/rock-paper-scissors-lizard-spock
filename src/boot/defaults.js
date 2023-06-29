5343ca77c7570dfdf4d27fe90fe9a08e63c79d4f
Marc Casadevall <mcasadevall@stxgroup.com>
2023-06-29 09:10:02 +0200

    Remove boot file

--- src/boot/defaults.js
+++
@@ -1,9 +0,0 @@
-import { boot } from 'quasar/wrappers'
-import { QCard } from 'quasar'
-
-export default boot(() => {
-  QCard.props.square = {
-    type: QCard.props.square,
-    default: true,
-  }
-})
