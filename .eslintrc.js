9446dd25ff50ceddcde00cbc008d917293efd96e
Marc Pont <mpont91@gmail.com>
2023-12-19 09:04:52 +0100

    Remove cypress for now

--- .eslintrc.js
+++ .eslintrc.js
@@ -29,8 +29,6 @@ module.exports = {
     // https://github.com/prettier/eslint-config-prettier#installation
     // usage with Prettier, provided by 'eslint-config-prettier'.
     'prettier',
-
-    'plugin:cypress/recommended',
   ],
 
   plugins: [
